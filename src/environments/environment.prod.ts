7a14919fc4b6cbcfd95f1b928aae84eecaeb13b0
Dakad <bpottered+github@gmail.com>
2018-06-25 01:18:11 +0200

    Init commit - ng g new

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  config: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
